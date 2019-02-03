bce8a37de4b2556ab4dbb7ecca59265ff6a96640
Medi Risasi <morisasy@gmail.com>
2019-02-03 14:56:14 +0200

    message and chatbot

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,6 +1,7 @@
 import React,
 	 {Component} from 'react';
-
+import axios from 'axios/index';
+import Message from './message';
 
 
 class  Chatbot extends Component {
@@ -11,11 +12,57 @@ class  Chatbot extends Component {
 		}
 	}
 
+	async df_text_query(text){
+		let says = {
+			speaks: 'me',
+			msg: {
+				text: {
+					text: text
+				}
+			}
+		};
+
+		this.setState({message: [...this.state.message, says]})
+		const res = await axios.post('/api/df_text_query', {text});
+
+		for (let msg of res.data.fulfillmentMessages){
+			let says = {
+				speaks: 'bot',
+				msg: msg 
+			}
+			this.setState({message: [...this.state.message, says]});
+		}
+	}
+	async df_event_query(event){
+		const res = await axios.post('/api/df_event_query', {event});
+
+		for (let msg of res.data.fulfillmentMessages){
+			let says = {
+				speaks: 'me',
+				msg: msg 
+			}
+			this.setState({message: [...this.state.message, says]});
+		}
+
+	}
+
+	renderMessages(stateMessages){
+		if (stateMessages){
+			return stateMessages.map((message, i) => {
+				return <Message key ={i} speaks = {message.speaks} text = {message.msg.text.text} />;
+			});
+		} else {
+			return null;
+		}
+
+	}
+
 	render(){
 		return (
 			<div style={{height: 400, width: 400, float: 'right'}}> 
 				<div id = "chatbot" style= {{height: '100%', width: '100%', overflow: 'auto'}}>
 					<h2>Chatbot says Hi!</h2>
+					{this.renderMessages(this.state.message)}
 					<input type = "text" />
 				</div>
 			</div>
