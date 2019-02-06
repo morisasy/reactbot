49bc150e613e08dcc2a48e4c68aed7a53ce4bfc9
Medi Risasi <morisasy@gmail.com>
2019-02-06 23:18:19 +0200

    Shop and time interval

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-require('./routes/dialogFlowRoutes')(app);
-// const dialogFlow = require('./routes/dialogFlowRoutes');
-// app.use('/dialog', dialogFlow);
+//require('./routes/dialogFlowRoutes')(app);
+ const dialogFlow = require('./routes/dialogFlowRoutes');
+ app.use('/dialog', dialogFlow);
 
 
 const PORT = process.env.PORT || 5000;
